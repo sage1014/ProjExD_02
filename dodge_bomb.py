e57be244531b62a0b15851420aa076a1605cb4c7
sage <58585454+sage1014@users.noreply.github.com>
2023-10-03 14:15:24 +0900

    練習問題1-1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,6 +1,6 @@
 import sys
 import pygame as pg
-
+import random
 
 WIDTH, HEIGHT = 1600, 900
 
@@ -8,11 +8,17 @@ WIDTH, HEIGHT = 1600, 900
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
+    enn = pg.Surface((20, 20))
+    pg.draw.circle(enn, (255, 0, 0), (10, 10), 10)
+    enn.set_colorkey((0, 0, 0))
+    bom_rect = enn.get_rect() # SurfaceからRectを抽出
+    x, y = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+    bom_rect.center = (x, y)
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     clock = pg.time.Clock()
-    
+
     tmr = 0
     while True:
         for event in pg.event.get():
@@ -21,6 +27,7 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        screen.blit(enn, bom_rect)
         pg.display.update()
         tmr += 1
         clock.tick(10)
